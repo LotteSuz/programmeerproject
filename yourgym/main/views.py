35e4af47bebd1c61f1abe033bd9e992f5838473a
Lotte Heek <lotteheek@gmail.com>
2019-06-05 11:56:50 +0200

    template

--- yourgym/main/views.py
+++ yourgym/main/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
+def index(request):
+    context = {
+        "pastas": "if you see this it works",
+    }
+    return render(request, "main/index.html", context)
