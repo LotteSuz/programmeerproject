a805762b0aa1c19f4f7a8da12bc869bf5f72033c
Lotte Heek <lotteheek@gmail.com>
2019-06-05 13:37:14 +0200

    dumbbell

--- yourgym/main/urls.py
+++ yourgym/main/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("/shop", views.shop, name="shop"),
+    path("/cart", views.cart, name="cart"),
+    path("/timetable", views.timetable, name="timetable"),
+    path("/schedule", views.schedule, name="schedule"),
+    path("/subscriptions", views.subscriptions, name="subscriptions"),
+    path("/register", views.register, name="register")
 ]
--- yourgym/main/views.py
+++ yourgym/main/views.py
@@ -8,6 +8,42 @@ from django.contrib.auth.decorators import user_passes_test
 # Create your views here.
 def index(request):
     context = {
-        "pastas": "if you see this it works",
+        "pastas": "this is the index page",
     }
     return render(request, "main/index.html", context)
+
+def shop(request):
+    context = {
+        "pastas": "this is the shop",
+    }
+    return render(request, "main/shop.html", context)
+
+def cart(request):
+    context = {
+        "pastas": "this is the shoppingcart",
+    }
+    return render(request, "main/cart.html", context)
+
+def timetable(request):
+    context = {
+        "pastas": "this is the timetable",
+    }
+    return render(request, "main/timetable.html", context)
+
+def schedule(request):
+    context = {
+        "pastas": "this is your schedule",
+    }
+    return render(request, "main/yourschedule.html", context)
+
+def subscriptions(request):
+    context = {
+        "pastas": "these are the subscriptions",
+    }
+    return render(request, "main/subscriptions.html", context)
+
+def register(request):
+    context = {
+        "pastas": "this is the register page",
+    }
+    return render(request, "main/register.html", context)
